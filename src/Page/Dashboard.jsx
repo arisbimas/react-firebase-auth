21cc2afe1a8ea90a9c983c4f8acd32c1274e5b81
arisbimas <arisbimas2@gmail.com>
2022-08-21 22:00:27 +0700

    minor style

--- src/Page/Dashboard.jsx
+++ src/Page/Dashboard.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import { UserAuth } from "../Contexts/AuthContext";
 
+const wrap = {
+  width: "100%",
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "white",
+};
+
+const btn = {
+  padding: "20px 25px",
+  border: "1px solid #eaeaea",
+  borderRadius: "10%",
+  backgroundColor: "coral",
+  cursor: "pointer",
+};
+
 export default function Dashboard() {
   const { logout } = UserAuth();
   const hanldeLogout = async () => {
@@ -8,8 +26,10 @@ export default function Dashboard() {
     await logout();
   };
   return (
-    <div style={{ background: "red" }}>
-      <button onClick={() => hanldeLogout()}>Logout</button>
+    <div style={wrap}>
+      <button style={btn} onClick={() => hanldeLogout()}>
+        Logout
+      </button>
     </div>
   );
 }
--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -4,6 +4,15 @@ import GoogleButton from "react-google-button";
 import { UserAuth } from "../Contexts/AuthContext";
 import { async } from "@firebase/util";
 import { Navigate } from "react-router-dom";
+
+const wrap = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "white",
+};
+
 export const Login = () => {
   const { googleSignIn, user } = UserAuth();
 
@@ -17,7 +26,9 @@ export const Login = () => {
   return (
     <div>
       <img src={logo} className="App-logo" alt="logo" />
-      <GoogleButton onClick={() => handleLogin()} />
+      <div style={wrap}>
+        <GoogleButton onClick={() => handleLogin()} />
+      </div>
     </div>
   );
 };
